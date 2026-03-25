07cdea97427c1f8ea5944a035bbf5e32ac401a2d
Juan <j.inzunza05@ufromail.cl>
2026-03-25 08:18:35 -0300

    .

--- src/busquedaBinaria.java
+++ src/busquedaBinaria.java
@@ -18,37 +18,6 @@ class Binario {
 
         int[][] matriz = new int[fil][col];
 
-        System.out.println(
-            "\n" + "Matriz de " + fil + " x " + col + " generada:" + "\n"
-        );
-        for (int i = 0; i < fil; i++) {
-            for (int j = 0; j < col; j++) {
-                matriz[i][j] = numGen.nextInt(1000) + 1;
-                System.out.print("\t" + matriz[i][j] + " ");
-            }
-            System.out.println();
-        }
-
-        System.out.print("\n" + "¿Que numero quieres buscar? ");
-        int buscar = sc.nextInt();
-        boolean encontrado = false;
-
-        for (int i = 0; i < fil; i++) {
-            for (int j = 0; j < col; j++) {
-                if (matriz[i][j] == buscar) {
-                    System.out.println(
-                        "\n" +
-                            "Encontrado en fila: " +
-                            (i + 1) +
-                            " columna: " +
-                            (j + 1)
-                    );
-                    encontrado = true;
-                }
-            }
-        }
-
-        if (!encontrado) System.out.println("El numero no está en la matriz");
         sc.close();
     }
 }
