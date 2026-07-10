39a7e5c88a90efc51ebda7f953f04c3553c74f21
Juan José Inzunza Rivas <j.inzunza05@ufromail.cl>
2026-07-10 15:19:54 -0400

    Delete solicitud.py

--- solicitud.py
+++
@@ -1,8 +0,0 @@
-class solicitud:
-    def __init__(self, id, ubicacion, prioridad, tipo, tiempo, equipos):
-        self.id = id
-        self.ubicacion = ubicacion
-        self.prioridad = prioridad
-        self.tipo = tipo
-        self.tiempo = tiempo
-        self.equipos = equipos
